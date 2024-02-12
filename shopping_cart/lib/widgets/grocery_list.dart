027c39e29927d9fe0a67da8c8d64bf6ebefa08e6
BrijeshSajeev <brijesh00300@gmail.com>
2024-02-12 20:18:49 +0530

    new:Error Handling

--- shopping_cart/lib/widgets/grocery_list.dart
+++ shopping_cart/lib/widgets/grocery_list.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:shopping_cart/data/categories.dart';
@@ -16,6 +17,7 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
+  String? _error;
   var _isLoading = true;
   @override
   void initState() {
@@ -25,28 +27,68 @@ class _GroceryListState extends State<GroceryList> {
   }
 
   void _loadItems() async {
-    final url = Uri.https("flutterstarter-c6659-default-rtdb.firebaseio.com",
-        'shopping-list.json');
-    final response = await http.get(url);
-    final Map<String, dynamic> listData = json.decode(response.body);
-    final List<GroceryItem> loadedList = [];
-    for (final item in listData.entries) {
-      final Category category = categories.entries
-          .firstWhere(
-              (catItem) => catItem.value.title == item.value['category'])
-          .value;
+    // final url = Uri.https("flutterstarter-c6659-default-rtdb.firebaseio.com",
+    //     'shopping-list.json');
+    // final response = await http.get(url);
+    // if (response.statusCode >= 400) {
+    //   setState(() {
+    //     _error = 'Failed to load data, status code: ${response.statusCode}';
+    //   });
+    // }
+    // final Map<String, dynamic> listData = json.decode(response.body);
+    // final List<GroceryItem> loadedList = [];
+    // for (final item in listData.entries) {
+    //   final Category category = categories.entries
+    //       .firstWhere(
+    //           (catItem) => catItem.value.title == item.value['category'])
+    //       .value;
 
-      loadedList.add(GroceryItem(
-        id: item.key,
-        name: item.value['name'],
-        quantity: item.value['quantity'],
-        category: category,
-      ));
+    //   loadedList.add(GroceryItem(
+    //     id: item.key,
+    //     name: item.value['name'],
+    //     quantity: item.value['quantity'],
+    //     category: category,
+    //   ));
+    // }
+    // setState(() {
+    //   _groceryItems = loadedList;
+    //   _isLoading = false;
+    // });
+    try {
+      final url = Uri.https("flutterstarter-c6659-default-rtdb.firebaseio.com",
+          'shopping-list.json');
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> listData = json.decode(response.body);
+        final List<GroceryItem> loadedList = [];
+
+        for (final item in listData.entries) {
+          final Category category = categories.entries
+              .firstWhere(
+                  (catItem) => catItem.value.title == item.value['category'])
+              .value;
+
+          loadedList.add(GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ));
+        }
+
+        setState(() {
+          _groceryItems = loadedList;
+          _isLoading = false;
+        });
+      } else {
+        throw Exception('${response.statusCode} Failed to load data');
+      }
+    } catch (e) {
+      setState(() {
+        _error = 'Error: $e';
+      });
     }
-    setState(() {
-      _groceryItems = loadedList;
-      _isLoading = false;
-    });
   }
 
   void _addItem() async {
@@ -63,11 +105,25 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _deleteItem(id) async {
+    final url = Uri.https("flutterstarter-c6659-default-rtdb.firebaseio.com",
+        'shopping-list/$id.json');
+    await http.delete(url);
+    _loadItems();
+  }
+
   @override
   Widget build(BuildContext context) {
     Widget content = const Center(
       child: Text("No items is added yet.."),
     );
+
+    if (_error != null) {
+      _isLoading = false;
+      content = Center(
+        child: Text(_error!),
+      );
+    }
 
     if (_isLoading) {
       content = const Center(
@@ -82,6 +138,7 @@ class _GroceryListState extends State<GroceryList> {
         itemBuilder: (context, index) => Dismissible(
           onDismissed: (direction) {
             setState(() {
+              _deleteItem(_groceryItems[index].id);
               _groceryItems.removeAt(index);
             });
           },
